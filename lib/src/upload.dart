d9ca8f76ae792e13e1783c65c5ab1e496b273188
殷绪来 <yinxulai@yinxulaideMacBook-Pro.local>
2019-02-28 00:16:54 +0800

    update

--- lib/src/upload.dart
+++ lib/src/upload.dart
@@ -1,12 +1,5 @@
-
 import 'package:http/http.dart';
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:http_parser/http_parser.dart';
-
-
-
 
 class Upload {
   Future upload() {}
@@ -14,16 +7,16 @@ class Upload {
   Future directUpload() {
     var uri = Uri.parse("http://pub.dartlang.org/packages/create");
     var request = new MultipartRequest("POST", uri);
-
-    var file = new MultipartFile.fromBytes('package',[10, 7, 23] ,contentType: new MediaType('application', 'x-tar'));
-
+    var file = new MultipartFile.fromBytes('package', [10, 7, 23],
+        contentType: new MediaType('application', 'x-tar'));
     request.fields['user'] = 'nweiz@google.com';
     request.files.add(file);
 
-    request.send().then((response) {
+    return request.send().then((response) {
       if (response.statusCode == 200) print("Uploaded!");
     });
   }
+
   // 分片
   Future resumeUpload() {}
 }
