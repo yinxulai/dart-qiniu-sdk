f4b3dc1737937acafb8f054b0b1fc93cc9215929
殷绪来 <yinxulai@yinxulaideMacBook-Pro.local>
2019-02-28 00:03:34 +0800

    添加上传

--- lib/src/upload.dart
+++ lib/src/upload.dart
@@ -1,10 +1,29 @@
 
 import 'package:http/http.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http_parser/http_parser.dart';
+
+
+
 
 class Upload {
   Future upload() {}
   // 直传
-  Future directUpload() {}
+  Future directUpload() {
+    var uri = Uri.parse("http://pub.dartlang.org/packages/create");
+    var request = new MultipartRequest("POST", uri);
+
+    var file = new MultipartFile.fromBytes('package',[10, 7, 23] ,contentType: new MediaType('application', 'x-tar'));
+
+    request.fields['user'] = 'nweiz@google.com';
+    request.files.add(file);
+
+    request.send().then((response) {
+      if (response.statusCode == 200) print("Uploaded!");
+    });
+  }
   // 分片
   Future resumeUpload() {}
 }
